e1459f5b5f02a04c9809f16dedf845599d6b42af
Prem <mp02233@otter02.eps.surrey.ac.uk>
2025-04-22 00:04:08 +0100

    Added swin model with batch size 8, lr=0.0001

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ STUDENT_NAME="Meenakshy Prem"
 # Set model-specific variables
 MODEL_NAME="swin_v2_b"
 LEARNING_RATE="0.0001"
-BATCH_SIZE="32"
+BATCH_SIZE="8"
 
 # Dynamically assign the weights based on model name
 if [ "$MODEL_NAME" == "efficientnet_v2_s" ]; then
